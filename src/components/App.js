2de8803cee94fd184f85eaf283e80d13692301c9
Jesse Ilc <jesseilc123@gmail.com>
2023-05-02 13:14:13 -0500

    Added db.json file and now able to access elements from that file

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,19 @@
 import Header from "./Header"
+import React, {useEffect, useState} from "react";
 
 function App() {
+  const [songs, setSongs] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:3000/songs")
+      .then(response => response.json())
+      .then((data) => setSongs(data))
+  }, [])
+
   return (
     <div>
       <Header />
+      <p>{songs.map((song)=> song.image)}</p>
     </div>
   );
 }
