af3492a54bbe40598a4cc544bcbbf6d7ac7f6ac8
Jesse Ilc <jesseilc123@gmail.com>
2023-05-02 12:05:05 -0500

    Added default export to header component and fixed minor issues, website is now running using npm start with no issues

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,9 @@ import React from "react";
 function Header() {
     return (
         <header>
-            <h1>Shuwahjus</h1>
+            <h1>Swagis</h1>
         </header>
-    )
+    );
 }
+
+export default Header;
