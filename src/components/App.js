a9692d0567a1c1c5aa475f1b17ccfe7d964037b8
Jesse Ilc <jesseilc123@gmail.com>
2023-05-02 13:18:35 -0500

    Added a Navbar however, not routes are set up yet

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,11 @@
 import Header from "./Header"
-import React, {useEffect, useState} from "react";
+import { Route, Switch } from "react-router-dom";
 
 function App() {
-  const [songs, setSongs] = useState([])
-
-  useEffect(() => {
-    fetch("http://localhost:3000/songs")
-      .then(response => response.json())
-      .then((data) => setSongs(data))
-  }, [])
 
   return (
     <div>
       <Header />
-      <p>{songs.map((song)=> song.image)}</p>
     </div>
   );
 }
---
+++ src/components/NavBar.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { NavLink } from "react-router-dom"
+
+function NavBar() {
+    return (
+    <div className="navbar">
+    </div>
+    )
+  }
+  
+  export default NavBar;
