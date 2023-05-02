df92bd1485b93336e79c7319f5aaf80651cfb338
Jesse Ilc <jesseilc123@gmail.com>
2023-05-02 13:43:37 -0500

    added style to the NavBar, this is only the inital stylying, will edit later

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,21 @@
 import React from "react";
 import { NavLink } from "react-router-dom"
 
+const linkStyles = {
+    display: "inline-block",
+    width: "50px",
+    padding: "12px",
+    margin: "0 6px 6px",
+    background: "blue",
+    textDecoration: "none",
+    color: "white",
+};
+
 function NavBar() {
     return (
     <div className="navbar">
-        <NavLink exact to="/songs">songs</NavLink>
-        <NavLink exact to="/">Home</NavLink>
+        <NavLink exact to="/songs" style={linkStyles}>songs</NavLink>
+        <NavLink exact to="/" style={linkStyles}>Home</NavLink>
     </div>
     )
   }
