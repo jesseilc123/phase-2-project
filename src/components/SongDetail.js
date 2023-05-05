935e6d8baeb253755a7fe8f1fe583adf3e5d7d14
Jesse Ilc <jesseilc123@gmail.com>
2023-05-05 11:59:00 -0500

    working on SongDetail, not functionally yet, added fetch and set up useStates.

--- src/components/SongDetail.js
+++ src/components/SongDetail.js
@@ -1,8 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 function SongDetail () {
+    const [indivSong, setIndivSong] = useState(null);
+    const params = useParams()
+
+    useEffect(() => {
+        fetch(`http://localhost:3000/songs/1`)
+            .then(r => r.json())
+            .then(data => setIndivSong(data))
+    }, [])
+
+    const {name, duration, genre } = indivSong
+
     return (
-        <div>hi</div>
+        <div>{name}</div>
     );
 };
 
