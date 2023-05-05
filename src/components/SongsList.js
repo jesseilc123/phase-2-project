385c13223640a146f128cb62bcd17b0ecc5096f9
Jesse Ilc <jesseilc123@gmail.com>
2023-05-05 10:47:41 -0500

    added useEffect with fetch to get data from db.json, working so far as intented.

--- src/components/SongsList.js
+++ src/components/SongsList.js
@@ -1,6 +1,15 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function SongsList() {
+    const [songs, setSongs] = useState([])
+
+    useEffect(() => {
+        fetch("http://localhost:3000/songs")
+            .then(r => r.json())
+            .then(data => setSongs(data))
+    }, [])
+
+    console.log(songs)
     return (
         <div>
             <h1>SongsList</h1>
